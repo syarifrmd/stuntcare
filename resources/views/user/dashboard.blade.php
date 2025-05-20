e608a8568e558f3de62ca60f9af8d2a207217944
syarif romadloni <syarifroma@gmail.com>
2025-05-20 11:44:14 +0700

    deleted:    database/migrations/2025_05_19_082245_add_role_to_users_table.php 	modified:   resources/views/user/dashboard.blade.php

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,17 +1,15 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("INI USER DAN BAKAL JADI STUNTCATRE WEBAPP") }}
-                </div>
-            </div>
-        </div>
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <x-app-layout>
+    <span name="header"></span>
+    <h1>INI MISAL</h1>
 </x-app-layout>
+</body>
+</html>
