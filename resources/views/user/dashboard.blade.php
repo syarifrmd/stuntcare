be78ad8bcdcd83c79ee5f04d44868676fbc150d9
syarif romadloni <syarifroma@gmail.com>
2025-05-22 23:41:46 +0700

    Pemantauan Gizi dan History

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
     <title>Document</title>
 </head>
 <body>
