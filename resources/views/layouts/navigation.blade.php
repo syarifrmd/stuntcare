5388fd539e7107aa3fd9eb72ed633b3429d4860a
syarif romadloni <syarifroma@gmail.com>
2025-05-24 22:08:55 +0700

    modified:   app/Http/Controllers/HistoriController.php 	modified:   resources/views/auth/login.blade.php 	modified:   resources/views/auth/register.blade.php 	modified:   resources/views/components/application-logo.blade.php 	modified:   resources/views/components/primary-button.blade.php 	modified:   resources/views/layouts/navigation.blade.php 	modified:   resources/views/pemantauan/index.blade.php 	modified:   resources/views/welcome.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,7 +28,7 @@
                         Home
                  </a>
                  <!--Artikel -->
-                <a href="{{ route('artikel.index') }}" class="flex items-center px-4 py-2 text-sm font-medium text-pink-500 bg-pink-50 border border-pink-300 rounded-full hover:bg-pink-100 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 transition">
+                <a href="{{ route('artikel.index') }}" class="flex items-center px-4 py-2 text-sm font-medium text-pink-500 bg-pink-50 border border-pink-300 rounded-full hover:bg-pink-100 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                     </svg>
