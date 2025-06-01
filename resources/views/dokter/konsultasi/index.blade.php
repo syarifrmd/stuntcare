75c186db84182adee103a7b30388fd3bac01a9ce
eekka <rizqiekapramudyatama@gmail.com>
2025-06-01 22:01:14 +0700

    back

--- resources/views/dokter/konsultasi/index.blade.php
+++ resources/views/dokter/konsultasi/index.blade.php
@@ -47,11 +47,21 @@
 </head>
 <body class="bg-gradient-to-l from-pink-50 to-pink-400/30 text-gray-800">
 
-    <header class="bg-pink-600 text-white text-center py-6 sm:py-8 shadow-lg">
+    <header class="bg-pink-600 text-white text-center py-6 sm:py-8 shadow-lg flex ">
+        <button onclick="window.history.back()" class=" relative text-gray-400 hover:text-gray-600 focus:outline-none pl-16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+            </svg>
+        </button>
+
+      
         <div class="container mx-auto px-4">
             <h1 class="text-2xl sm:text-3xl lg:text-4xl font-bold font-montserrat">Kelola Jadwal Konsultasi Anda</h1>
             <p class="mt-1 sm:mt-2 text-sm sm:text-base opacity-90">Lihat, tambah, edit, hapus, dan konfirmasi jadwal konsultasi.</p>
         </div>
+
+       
+       
     </header>
 
     <main class="container mx-auto py-8 px-4 sm:px-6 lg:px-8" x-data="dokterJadwalData()">
